f2f3e2778e22475ff5384f30446acdd7eca7441f
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-09-19 13:49:00 -0300

    Add files via upload

--- in progress/workingWithStrings01.js
+++ in progress/workingWithStrings01.js
@@ -19,3 +19,21 @@ console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
 
 console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat');
+  } else console.log('You got lucky');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
