349d8b3e9dff2a97778d28acdacd7cfbbb7c6f86
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-10-05 14:17:10 -0300

    Add files via upload

--- in progress/workingWithStrings02.js
+++ in progress/workingWithStrings02.js
@@ -27,3 +27,36 @@ function fixEmail(email) {
 }
 
 console.log(fixEmail(loginEmail));
+
+// Replacing
+
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23.';
+
+console.log(announcement.replaceAll('door', 'gate'));
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+// Practice exercice
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
