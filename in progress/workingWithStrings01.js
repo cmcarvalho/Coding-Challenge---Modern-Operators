1bb7ae1ab972439e7c0e355eaa3f081c0ed12a01
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2022-08-24 11:50:04 -0300

    Add files via upload

--- in progress/workingWithStrings01.js
+++ in progress/workingWithStrings01.js
@@ -1,47 +1,70 @@
 'use strict';
 
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
+
+// console.log(typeof plane[0]);
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log(plane[3]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B373'.length);
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
+// console.log(airline.indexOf('portugal'));
+// console.log(airline.indexOf('TAP'));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function (seat) {
+//   // B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') {
+//     console.log('You got the middle seat');
+//   } else console.log('You got lucky');
+// };
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// // JavaScript converts a String to a Object to use their string methods.
+// console.log(new String('jonas'));
+// console.log(typeof new String('jonas'));
+
+// console.log(typeof new String('jonas').slice(1));
+
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
-console.log(typeof plane[0]);
 console.log(plane[0]);
 console.log(plane[1]);
 console.log(plane[2]);
 console.log(plane[3]);
+// console.log(plane[-1]);
 console.log('B737'[0]);
 
 console.log(airline.length);
-console.log('B373'.length);
+console.log('B737'.length);
 
 console.log(airline.indexOf('r'));
 console.log(airline.lastIndexOf('r'));
 console.log(airline.indexOf('Portugal'));
-console.log(airline.indexOf('portugal'));
-console.log(airline.indexOf('TAP'));
 
 console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
 
 console.log(airline.slice(0, airline.indexOf(' ')));
-console.log(airline.slice(airline.lastIndexOf(' ') + 1));
-
-console.log(airline.slice(-2));
-console.log(airline.slice(1, -1));
-
-const checkMiddleSeat = function (seat) {
-  // B and E are middle seats
-  const s = seat.slice(-1);
-  if (s === 'B' || s === 'E') {
-    console.log('You got the middle seat');
-  } else console.log('You got lucky');
-};
-
-checkMiddleSeat('11B');
-checkMiddleSeat('23C');
-checkMiddleSeat('3E');
-
-// JavaScript converts a String to a Object to use their string methods.
-console.log(new String('jonas'));
-console.log(typeof new String('jonas'));
-
-console.log(typeof new String('jonas').slice(1));
+console.log(airline.slice(lastIndexOf(' ')));
